586945e76f4d4882a364e211338b6649281a2ae6
Molamin <Molamin73@gmail.com>
2019-02-17 23:27:20 +0000

    added necessary routes

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('/projects', 'ProjectsController@index');
 Route::get('/projects/{project}', 'ProjectsController@show');
 
 Route::post('/projects','ProjectsController@store'
-);
+)->middleware('auth');
 
