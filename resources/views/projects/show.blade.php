a4a860f6034d87f1bc5aaa56e51889f1a745064a
Molamin <Molamin73@gmail.com>
2019-02-18 03:52:55 +0000

    inherit from layout

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-
-      
-    </head>
-    <body>
+@section('content')
        <h1>{{ $project->title}} </h1>
        <div>{{ $project->description}} </div>
-    </body>
-</html>
+       <a href="/projects"> Go Back</a>
+@endsection
