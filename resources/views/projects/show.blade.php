22f931d84154464f49b133782711badaefc4cc82
mola <molamin73@gmail.com>
2019-02-27 01:08:16 +0000

    added activity feeds

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -73,10 +73,11 @@
                         @endif
                      </div>
                  </div>
-                 
-                 <div class="lg:w-1/4 px-3 ">
-                    @include('projects.card')
-                 </div>
+                 <div class="lg:w-1/4 px-3 lg:py-8">
+                @include ('projects.card')
+                @include ('projects.activity.card')
+            </div>
+               
          </div>
 </main>
     
